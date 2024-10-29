732dcccee0c6b84ef075eecbfdc9a7bb5e7ff24c
mahhis <vasiukovich03@gmail.com>
2024-10-29 17:54:50 +0300

    c

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
-import 'index.css'
+import './index.css'
 import { ThemeProvider, createTheme } from '@mui/material'
 import { render } from 'preact'
-import App from 'App'
+import App from './App'
 
 const theme = createTheme({
     palette: {
