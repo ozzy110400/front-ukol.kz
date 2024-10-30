5bb01fa2f8d8063308399955d03c0af1879b5b0d
mahhis <vasiukovich03@gmail.com>
2024-10-30 10:56:19 +0300

    t6

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -27,86 +27,88 @@ function ResponsiveAppBar() {
 
 
   return (
-    <AppBar position="static" sx={{ backgroundColor: 'transparent', mt: 2 }} elevation={0} >
-      <Container maxWidth="xl">
-        <Toolbar disableGutters>
-        <Box
-            sx={{ display: { xs: 'flex', md: 'flex' }, mr: 1,  }}
-          >
-            <img
-              src="/img/logo.png" 
-              alt="Logo"
-              style={{ maxHeight: '80px', width: 'auto' }} 
-            />
-          </Box>
-          <Typography
-            variant="h6"
-            noWrap
-            component="a"
-            href="#app-bar-with-responsive-menu"
-            sx={{
-              mr: 2,
+    <Box>1234</Box>
+
+    // <AppBar position="static" sx={{ backgroundColor: 'transparent', mt: 2 }} elevation={0} >
+    //   <Container maxWidth="xl">
+    //     <Toolbar disableGutters>
+    //     <Box
+    //         sx={{ display: { xs: 'flex', md: 'flex' }, mr: 1,  }}
+    //       >
+    //         <img
+    //           src="/img/logo.png" 
+    //           alt="Logo"
+    //           style={{ maxHeight: '80px', width: 'auto' }} 
+    //         />
+    //       </Box>
+    //       <Typography
+    //         variant="h6"
+    //         noWrap
+    //         component="a"
+    //         href="#app-bar-with-responsive-menu"
+    //         sx={{
+    //           mr: 2,
     
-            }}
-          >
-            UKOL.KZ
-          </Typography>
+    //         }}
+    //       >
+    //         UKOL.KZ
+    //       </Typography>
          
-          <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' }, justifyContent: 'center'  }}>
-            {pages.map((page) => (
-              <Button
-                key={page}
-                onClick={handleCloseNavMenu}
-                sx={{ my: 2, display: 'block', flex: 1 }}
-              >
-                <Typography variant="h5">
-                {page}
-                </Typography>
-              </Button>
-            ))}
-          </Box>
-          <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
+    //       <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' }, justifyContent: 'center'  }}>
+    //         {pages.map((page) => (
+    //           <Button
+    //             key={page}
+    //             onClick={handleCloseNavMenu}
+    //             sx={{ my: 2, display: 'block', flex: 1 }}
+    //           >
+    //             <Typography variant="h5">
+    //             {page}
+    //             </Typography>
+    //           </Button>
+    //         ))}
+    //       </Box>
+    //       <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
            
-          </Box>
-          <Box sx={{ flexGrow: 0, display: { xs: 'flex', md: 'none' } }} >
-          <IconButton
-              size="large"
-              aria-label="account of current user"
-              aria-controls="menu-appbar"
-              aria-haspopup="true"
-              onClick={handleOpenNavMenu}
-             color='primary'
-             sx={{ color: '#000'  }}
+    //       </Box>
+    //       <Box sx={{ flexGrow: 0, display: { xs: 'flex', md: 'none' } }} >
+    //       <IconButton
+    //           size="large"
+    //           aria-label="account of current user"
+    //           aria-controls="menu-appbar"
+    //           aria-haspopup="true"
+    //           onClick={handleOpenNavMenu}
+    //          color='primary'
+    //          sx={{ color: '#000'  }}
              
-            >
-              <MenuIcon sx={{ fontSize: '2.5rem',  fontWeight: 'bold' }}/>
-            </IconButton>
-            <Menu
-              sx={{ mt: '45px' }}
-              id="menu-appbar"
-              anchorEl={anchorElNav}
-              anchorOrigin={{
-                vertical: 'top',
-                horizontal: 'right',
-              }}
-              keepMounted
-              transformOrigin={{
-                vertical: 'top',
-                horizontal: 'right',
-              }}
-              open={Boolean(anchorElNav)}
-              onClose={handleCloseNavMenu}
-            >
-              {settings.map((setting) => (
-                <MenuItem key={setting} onClick={handleCloseNavMenu}>
-                  <Typography variant='h5' sx={{ textAlign: 'center' }}>{setting}</Typography>
-                </MenuItem>
-              ))}
-            </Menu>
-          </Box>
-        </Toolbar>
-      </Container>
-    </AppBar>
+    //         >
+    //           <MenuIcon sx={{ fontSize: '2.5rem',  fontWeight: 'bold' }}/>
+    //         </IconButton>
+    //         <Menu
+    //           sx={{ mt: '45px' }}
+    //           id="menu-appbar"
+    //           anchorEl={anchorElNav}
+    //           anchorOrigin={{
+    //             vertical: 'top',
+    //             horizontal: 'right',
+    //           }}
+    //           keepMounted
+    //           transformOrigin={{
+    //             vertical: 'top',
+    //             horizontal: 'right',
+    //           }}
+    //           open={Boolean(anchorElNav)}
+    //           onClose={handleCloseNavMenu}
+    //         >
+    //           {settings.map((setting) => (
+    //             <MenuItem key={setting} onClick={handleCloseNavMenu}>
+    //               <Typography variant='h5' sx={{ textAlign: 'center' }}>{setting}</Typography>
+    //             </MenuItem>
+    //           ))}
+    //         </Menu>
+    //       </Box>
+    //     </Toolbar>
+    //   </Container>
+    // </AppBar>
   );
 }
 export default ResponsiveAppBar;
