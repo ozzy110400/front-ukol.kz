4211965063911bc7ddfdcbcb22ef9f21c9cb0901
mahhis <vasiukovich03@gmail.com>
2024-10-30 11:17:02 +0300

    t9

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -10,105 +10,97 @@ import Button from '@mui/material/Button';
 import MenuItem from '@mui/material/MenuItem';
 import { useState } from 'react';
 
-
 const pages = ['Products', 'Pricing', 'Blog'];
 const settings = ['Profile', 'Account', 'Dashboard', 'Logout'];
 
 function ResponsiveAppBar() {
-  const [anchorElNav, setAnchorElNav] = useState<null | HTMLElement>(null);
+  const [openNav, setOpenNav] = useState(false);
 
-  const handleOpenNavMenu = (event: React.MouseEvent<HTMLElement>) => {
-    setAnchorElNav(event.currentTarget);
+  const handleOpenNavMenu = () => {
+    setOpenNav(true);
   };
 
   const handleCloseNavMenu = () => {
-    setAnchorElNav(null);
+    setOpenNav(false);
   };
 
-
   return (
-    <Box>1234</Box>
-
-    // <AppBar position="static" sx={{ backgroundColor: 'transparent', mt: 2 }} elevation={0} >
-    //   <Container maxWidth="xl">
-    //     <Toolbar disableGutters>
-    //     <Box
-    //         sx={{ display: { xs: 'flex', md: 'flex' }, mr: 1,  }}
-    //       >
-    //         <img
-    //           src="/img/logo.png" 
-    //           alt="Logo"
-    //           style={{ maxHeight: '80px', width: 'auto' }} 
-    //         />
-    //       </Box>
-    //       <Typography
-    //         variant="h6"
-    //         noWrap
-    //         component="a"
-    //         href="#app-bar-with-responsive-menu"
-    //         sx={{
-    //           mr: 2,
-    
-    //         }}
-    //       >
-    //         UKOL.KZ
-    //       </Typography>
+    <AppBar position="static" sx={{ backgroundColor: 'transparent', mt: 2 }} elevation={0} >
+      <Container maxWidth="xl">
+        <Toolbar disableGutters>
+          <Box sx={{ display: { xs: 'flex', md: 'flex' }, mr: 1 }}>
+            <img
+              src="/img/logo.png" 
+              alt="Logo"
+              style={{ maxHeight: '80px', width: 'auto' }} 
+            />
+          </Box>
+          <Typography
+            variant="h6"
+            noWrap
+            component="a"
+            href="#app-bar-with-responsive-menu"
+            sx={{
+              mr: 2,
+            }}
+          >
+            UKOL.KZ
+          </Typography>
          
-    //       <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' }, justifyContent: 'center'  }}>
-    //         {pages.map((page) => (
-    //           <Button
-    //             key={page}
-    //             onClick={handleCloseNavMenu}
-    //             sx={{ my: 2, display: 'block', flex: 1 }}
-    //           >
-    //             <Typography variant="h5">
-    //             {page}
-    //             </Typography>
-    //           </Button>
-    //         ))}
-    //       </Box>
-    //       <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
-           
-    //       </Box>
-    //       <Box sx={{ flexGrow: 0, display: { xs: 'flex', md: 'none' } }} >
-    //       <IconButton
-    //           size="large"
-    //           aria-label="account of current user"
-    //           aria-controls="menu-appbar"
-    //           aria-haspopup="true"
-    //           onClick={handleOpenNavMenu}
-    //          color='primary'
-    //          sx={{ color: '#000'  }}
-             
-    //         >
-    //           <MenuIcon sx={{ fontSize: '2.5rem',  fontWeight: 'bold' }}/>
-    //         </IconButton>
-    //         <Menu
-    //           sx={{ mt: '45px' }}
-    //           id="menu-appbar"
-    //           anchorEl={anchorElNav}
-    //           anchorOrigin={{
-    //             vertical: 'top',
-    //             horizontal: 'right',
-    //           }}
-    //           keepMounted
-    //           transformOrigin={{
-    //             vertical: 'top',
-    //             horizontal: 'right',
-    //           }}
-    //           open={Boolean(anchorElNav)}
-    //           onClose={handleCloseNavMenu}
-    //         >
-    //           {settings.map((setting) => (
-    //             <MenuItem key={setting} onClick={handleCloseNavMenu}>
-    //               <Typography variant='h5' sx={{ textAlign: 'center' }}>{setting}</Typography>
-    //             </MenuItem>
-    //           ))}
-    //         </Menu>
-    //       </Box>
-    //     </Toolbar>
-    //   </Container>
-    // </AppBar>
+          <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' }, justifyContent: 'center' }}>
+            {pages.map((page) => (
+              <Button
+                key={page}
+                onClick={handleCloseNavMenu}
+                sx={{ my: 2, display: 'block', flex: 1 }}
+              >
+                <Typography variant="h5">
+                  {page}
+                </Typography>
+              </Button>
+            ))}
+          </Box>
+          <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
+
+          </Box>
+          <Box sx={{ flexGrow: 0, display: { xs: 'flex', md: 'none' } }}>
+            <IconButton
+              size="large"
+              aria-label="account of current user"
+              aria-controls="menu-appbar"
+              aria-haspopup="true"
+              onClick={handleOpenNavMenu}
+              color='primary'
+              sx={{ color: '#000' }}
+            >
+              <MenuIcon sx={{ fontSize: '2.5rem', fontWeight: 'bold' }} />
+            </IconButton>
+            <Menu
+              sx={{ mt: '45px' }}
+              id="menu-appbar"
+              anchorOrigin={{
+                vertical: 'top',
+                horizontal: 'right',
+              }}
+              keepMounted
+              transformOrigin={{
+                vertical: 'top',
+                horizontal: 'right',
+              }}
+              open={openNav}
+              onClose={handleCloseNavMenu}
+            >
+              {settings.map((setting) => (
+                <MenuItem key={setting} onClick={handleCloseNavMenu}>
+                  <Typography variant='h5' sx={{ textAlign: 'center' }}>{setting}</Typography>
+                </MenuItem>
+              ))}
+            </Menu>
+          </Box>
+        </Toolbar>
+      </Container>
+    </AppBar>
   );
 }
+
 export default ResponsiveAppBar;
