acf1e00ef426749093c38f59a3774f8a4f3aa13f
mahhis <vasiukovich03@gmail.com>
2024-11-19 14:20:17 +0300

    updates FAQ

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
-import { Suspense } from 'preact/compat'
-import { Switch, Route} from 'wouter-preact';
+import { Suspense, useEffect } from 'preact/compat'
+import { Switch, Route, useLocation} from 'wouter-preact';
 import Header from './components/Header'
 import Footer from './components/Footer';
 import Main from './pages/Main';
@@ -7,6 +7,25 @@ import Order from './pages/Order';
 import Account from './pages/Account';
 
 export default function () {
+
+  const [location] = useLocation(); // Get the current route
+
+  useEffect(() => {
+    switch (location) {
+      case '/':
+        document.title = 'Главная страница';
+        break;
+      case '/order':
+        document.title = 'Заказ медсотрудника';
+        break;
+      case '/account':
+        document.title = 'Личный кабинет';
+        break;
+      default:
+        document.title = 'Ukol';
+    }
+  }, [location]); 
+
 
   return (
     <div className="container mx-auto" style={{ paddingBottom: '72px' }}>      <Suspense fallback={<p>Loading...</p>} >
--- src/components/FAQ.tsx
+++ src/components/FAQ.tsx
@@ -36,12 +36,11 @@ export default function FAQ() {
           id="panel1-header"
         >
           <Typography variant="h5">
-            Accordion 1
+          1.⁠ ⁠Через сколько времени медсестра приедет на дом?
           </Typography>
         </AccordionSummary>
         <AccordionDetails>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
-          malesuada lacus ex, sit amet blandit leo lobortis eget.
+        Медсестры Ukol.kz обычно приезжают в течение 30-40 минут после подтверждения вашего заказа. Точное время зависит от загруженности и расстояния до вашего адреса.
         </AccordionDetails>
       </Accordion>
       <Accordion sx={{
@@ -56,12 +55,11 @@ export default function FAQ() {
           id="panel2-header"
         >
           <Typography variant="h5">
-            Accordion 2
+          2.⁠ ⁠Какие состояния требуют капельницы или уколов, которые вы предлагаете?
           </Typography>
         </AccordionSummary>
         <AccordionDetails>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
-          malesuada lacus ex, sit amet blandit leo lobortis eget.
+        Мы оказываем помощь в случаях обезвоживания, необходимости восстановления после интоксикации, витаминной поддержки, а также при медицинских назначениях для лечения заболеваний. Все процедуры выполняются строго по показаниям.
         </AccordionDetails>
       </Accordion>
       <Accordion sx={{
@@ -76,13 +74,48 @@ export default function FAQ() {
           id="panel3-header"
         >
           <Typography variant="h5">
-            Accordion 3
+          3.⁠ ⁠Гарантируете ли вы конфиденциальность при предоставлении услуг?
           </Typography>
         </AccordionSummary>
         <AccordionDetails>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
-          malesuada lacus ex, sit amet blandit leo lobortis eget.
+        Да, мы строго соблюдаем принципы конфиденциальности. Вся информация о ваших обращениях и состоянии здоровья остаётся закрытой и защищённой.
         </AccordionDetails>
+      </Accordion>
+      <Accordion sx={{
+        mt:1,
+        mr:2,
+        ml:2,
+        border: '1px solid black'
+      }}>
+        <AccordionSummary
+          expandIcon={<ExpandMoreIcon />}
+          aria-controls="panel3-content"
+          id="panel3-header"
+        >
+          <Typography variant="h5">
+          4.⁠ ⁠Как происходит расчет стоимости услуг?
+          </Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+        Стоимость рассчитывается автоматически с учётом вашего местоположения, времени заказа и выбранной услуги. Мы стараемся предоставлять лучшие цены!        </AccordionDetails>
+      </Accordion>
+      <Accordion sx={{
+        mt:1,
+        mr:2,
+        ml:2,
+        border: '1px solid black'
+      }}>
+        <AccordionSummary
+          expandIcon={<ExpandMoreIcon />}
+          aria-controls="panel3-content"
+          id="panel3-header"
+        >
+          <Typography variant="h5">
+          5.⁠ ⁠Кто выполняет процедуры?
+          </Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+        Все процедуры выполняются только сертифицированными медсестрами с соответствующей квалификацией. После оказания услуги вы сможете оставить отзыв и оценку для медработника.        </AccordionDetails>
       </Accordion>
     </div>
   );
