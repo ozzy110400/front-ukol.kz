d0011b0182b4566ff386a73133bbf50574b88012
mahhis <vasiukovich03@gmail.com>
2024-12-14 20:16:56 +0300

    upd

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -1,11 +1,6 @@
 import {
   Box,
-  Button,
-  Typography,
 } from '@mui/material';
-import { useAtom } from 'jotai';
-import currentOrderAtom from '../atoms/currentOrder';
-
 import MainPhrase from '../components/main/MainPhrase';
 import FAQ from '../components/main/FAQ';
 import Header from 'components/main/Header';
@@ -14,15 +9,6 @@ import WeOffer from 'components/main/WeOffer';
 import Contact from 'components/main/Contacts';
 
 export default function Main() {
-
-  const handleWhatsAppClick = () => {
-    const phone = '77027776776'; // Replace with the actual phone number
-    const message = 'Здравствуйте! Нужна помощь с заказом медицинских услуг '; // The message you want to send
-    const whatsappUrl = `https://api.whatsapp.com/send?phone=${phone}&text=${encodeURIComponent(message)}`;
-    
-    window.open(whatsappUrl, '_blank'); // Open in a new tab
-  };
-
   return (
     <Box sx={{ backgroundColor: 'transparent' , justifyContent: 'center', textAlign: 'center',}}>
       <Header/>
@@ -31,42 +17,6 @@ export default function Main() {
       <WeOffer/>
       <FAQ/>
       <Contact/>
-
-      {/* <Typography variant="h5">
-        Нужна помощь? 
-      </Typography>
-      <Button
-           variant="contained"
-           onClick={handleWhatsAppClick}
-           sx={{ 
-            textTransform: 'none',
-            backgroundColor: '#88e788', 
-            border: '3px solid black', 
-            borderRadius: '140px',
-            margin: 2,
-         }}
-      >
-        <Box 
-          component="img" 
-          src={'/img/wa.png'} 
-          alt="WhatsApp Icon" 
-          sx={{
-            width: 24,    // Set the width and height as desired
-            height: 24,
-            marginRight: 1, // Adjust spacing between icon and text
-          }}
-        />
-        <Typography variant="h5"  sx={{
-              fontSize: {
-                xs: '1rem',   // Font size for small screens
-                sm: '1.2rem',     // Font size for medium screens
-                md: '1.4rem',   // Font size for large screens
-              },
-             }}>
-          Нажмите для консультация 
-        </Typography>
-          </Button> 
-          <FAQ/>*/}
     </Box>
   );
 }
--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,16 @@ import { defineConfig, Plugin } from 'vite'
 import preact from '@preact/preset-vite'
 import tsconfigPaths from 'vite-tsconfig-paths'
 import { visualizer } from 'rollup-plugin-visualizer'
+import compression from 'vite-plugin-compression';
 
 export default defineConfig({
-  plugins: [preact(), tsconfigPaths()],
+  plugins: [
+    preact(),
+    tsconfigPaths(), 
+    compression({
+        algorithm: 'gzip', // or 'brotliCompress'
+        ext: '.gz', // or '.br' for Brotli
+      }),],
   resolve: {
     alias: {
       'react': 'preact/compat',
