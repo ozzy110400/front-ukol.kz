db7553524fd1a2fe6ea2ab6ee370c5f98586c707
mahhis <vasiukovich03@gmail.com>
2025-01-14 13:00:41 +0300

    updt

--- src/components/main/Header.tsx
+++ src/components/main/Header.tsx
@@ -64,7 +64,7 @@ function Header() {
             variant="h6"
             noWrap
             component="a"
-            href="/"
+            href="#app-bar-with-responsive-menu"
             sx={{
               mr: 2,
             }}
